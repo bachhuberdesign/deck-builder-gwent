8226af906b32cf1f2e22245172fe4196e3cbf0af
Eric Bachhuber <bachhuberdesign@gmail.com>
2017-03-27 09:44:05 -0500

    Preparing for faction select UI

---
+++ app/src/main/java/com/bachhuberdesign/gwentcardviewer/features/factionselect/FactionSelectItem.kt
@@ -0,0 +1,10 @@
+package com.bachhuberdesign.gwentcardviewer.features.factionselect
+
+/**
+ * @author Eric Bachhuber
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+class FactionSelectItem {
+
+}
---
+++ app/src/main/java/com/bachhuberdesign/gwentcardviewer/features/factionselect/FactionSelectPresenter.kt
@@ -0,0 +1,19 @@
+package com.bachhuberdesign.gwentcardviewer.features.factionselect
+
+import com.bachhuberdesign.gwentcardviewer.features.shared.base.BasePresenter
+import com.bachhuberdesign.gwentcardviewer.inject.annotation.PersistedScope
+
+/**
+ * @author Eric Bachhuber
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+@PersistedScope
+class FactionSelectPresenter : BasePresenter<FactionSelectMvpContract>() {
+
+    companion object {
+        @JvmStatic val TAG: String = this::class.java.name
+    }
+
+
+}
