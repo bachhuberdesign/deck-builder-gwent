bb54f3261466de6abc0c76a44f022beb0991f9c8
Eric Bachhuber <bachhuberdesign@gmail.com>
2017-04-06 16:55:51 -0500

    Updating faction/leader select UI and added internet permission

--- app/src/main/java/com/bachhuberdesign/gwentcardviewer/features/factionselect/FactionSelectItem.kt
+++ app/src/main/java/com/bachhuberdesign/gwentcardviewer/features/factionselect/FactionSelectItem.kt
@@ -1,7 +1,7 @@
 package com.bachhuberdesign.gwentcardviewer.features.factionselect
 
-import android.net.Uri
 import android.support.v7.widget.RecyclerView
+import android.util.Log
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
@@ -9,8 +9,7 @@ import com.bachhuberdesign.gwentcardviewer.R
 import com.bachhuberdesign.gwentcardviewer.features.shared.model.Card
 import com.bumptech.glide.Glide
 import com.mikepenz.fastadapter.items.AbstractItem
-import kotlinx.android.synthetic.main.faction_select_item_old.view.*
-
+import kotlinx.android.synthetic.main.faction_select_item.view.*
 
 /**
  * @author Eric Bachhuber
@@ -18,6 +17,10 @@ import kotlinx.android.synthetic.main.faction_select_item_old.view.*
  * @since 1.0.0
  */
 class FactionSelectItem : AbstractItem<FactionSelectItem, FactionSelectItem.ViewHolder>() {
+
+    companion object {
+        @JvmStatic val TAG: String = this::class.java.name
+    }
 
     var backgroundUrl = ""
     var factionName = ""
@@ -42,27 +45,21 @@ class FactionSelectItem : AbstractItem<FactionSelectItem, FactionSelectItem.View
         viewHolder.name.text = factionName
         viewHolder.description.text = factionDescription
 
-//        Glide.clear(viewHolder.factionImage)
         Glide.clear(viewHolder.leader1)
         Glide.clear(viewHolder.leader2)
         Glide.clear(viewHolder.leader3)
 
-//        Glide.with(viewHolder.itemView.context)
-//                .load(backgroundUrl)
-//                .into(viewHolder.factionImage)
-
         Glide.with(viewHolder.itemView.context)
-                .load(Uri.parse("file:///android_asset/leader.png"))
-                .fitCenter()
+                .load(leaders?.get(0)?.iconUrl)
                 .into(viewHolder.leader1)
 
         Glide.with(viewHolder.itemView.context)
-                .load(Uri.parse("file:///android_asset/leader.png"))
+                .load(leaders?.get(1)?.iconUrl)
                 .fitCenter()
                 .into(viewHolder.leader2)
 
         Glide.with(viewHolder.itemView.context)
-                .load(Uri.parse("file:///android_asset/leader.png"))
+                .load(leaders?.get(2)?.iconUrl)
                 .fitCenter()
                 .into(viewHolder.leader3)
     }
@@ -81,7 +78,6 @@ class FactionSelectItem : AbstractItem<FactionSelectItem, FactionSelectItem.View
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         var name: TextView = view.faction_name_text
         var description: TextView = view.faction_description_text
-//        var factionImage: ImageView = view.faction_image
 
         var leader1: ImageView = view.leader1_image
         var leader2: ImageView = view.leader2_image
