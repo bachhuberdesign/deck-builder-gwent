20db7293767b50604f85d8906d5df6c0eda375a7
Eric Bachhuber <bachhuberdesign@gmail.com>
2017-04-07 17:21:16 -0500

    Added RecyclerView and item for displaying cards lists

---
+++ app/src/main/java/com/bachhuberdesign/gwentcardviewer/features/cardviewer/CardItem.kt
@@ -0,0 +1,65 @@
+package com.bachhuberdesign.gwentcardviewer.features.cardviewer
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import com.bachhuberdesign.gwentcardviewer.R
+import com.bachhuberdesign.gwentcardviewer.features.shared.model.Card
+import com.bachhuberdesign.gwentcardviewer.features.shared.model.Faction
+import com.bachhuberdesign.gwentcardviewer.util.getStringResourceByName
+import com.bumptech.glide.Glide
+import com.mikepenz.fastadapter.items.AbstractItem
+import kotlinx.android.synthetic.main.card_item.view.*
+
+/**
+ * @author Eric Bachhuber
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+class CardItem : AbstractItem<CardItem, CardItem.ViewHolder>() {
+
+    companion object {
+        @JvmStatic val TAG: String = this::class.java.name
+    }
+
+    var card: Card? = null
+
+    override fun getLayoutRes(): Int {
+        return R.layout.card_item
+    }
+
+    override fun getViewHolder(v: View): ViewHolder {
+        return ViewHolder(v)
+    }
+
+    override fun getType(): Int {
+        return 888
+    }
+
+    override fun bindView(holder: ViewHolder, payloads: MutableList<Any>) {
+        super.bindView(holder, payloads)
+
+        Glide.clear(holder.cardImage)
+        Glide.with(holder.itemView.context)
+                .load(card?.iconUrl)
+                .into(holder.cardImage)
+
+        holder.name.text = card?.name
+        holder.faction.text = holder.itemView.context.getStringResourceByName(Faction.ID_TO_KEY.apply(card?.faction))
+        holder.description.text = card?.description
+        holder.power.text = card?.power.toString()
+    }
+
+    override fun unbindView(holder: ViewHolder) {
+        super.unbindView(holder)
+        Glide.clear(holder.cardImage)
+    }
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        var name = view.card_name_text
+        var faction = view.faction_name_text
+        var description = view.card_description_text
+        var power = view.card_power_text
+        var cardImage = view.card_image
+    }
+
+}
