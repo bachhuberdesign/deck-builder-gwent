c5d24069be33959b3635bd8525241e13ce3230b3
Eric Bachhuber <bachhuberdesign@gmail.com>
2017-04-12 09:07:39 -0500

    Added MaterialDrawer to MainActivity

--- app/src/main/java/com/bachhuberdesign/gwentcardviewer/MainActivity.kt
+++ app/src/main/java/com/bachhuberdesign/gwentcardviewer/MainActivity.kt
@@ -6,8 +6,13 @@ import com.bachhuberdesign.gwentcardviewer.features.deckbuild.DeckbuildControlle
 import com.bachhuberdesign.gwentcardviewer.features.factionselect.FactionSelectController
 import com.bachhuberdesign.gwentcardviewer.features.shared.base.BaseActivity
 import com.bluelinelabs.conductor.Conductor
+import com.bluelinelabs.conductor.Controller
 import com.bluelinelabs.conductor.Router
 import com.bluelinelabs.conductor.RouterTransaction
+import com.mikepenz.materialdrawer.DrawerBuilder
+import com.mikepenz.materialdrawer.model.DividerDrawerItem
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem
+import com.mikepenz.materialdrawer.model.SecondaryDrawerItem
 import kotlinx.android.synthetic.main.activity_main.*
 
 /**
@@ -27,24 +32,61 @@ class MainActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
 
         router = Conductor.attachRouter(this, container, savedInstanceState)
 
         if (!router.hasRootController()) {
             Log.d(TAG, "Setting root controller")
 
-            if (BuildConfig.DEBUG) {
+            // TODO: Show drawer
+
+            if (!BuildConfig.DEBUG) {
                 router.setRoot(RouterTransaction.with(DeckbuildController(1)))
             } else {
                 router.setRoot(RouterTransaction.with(FactionSelectController()))
             }
         }
+
+        initNavigationDrawer()
     }
 
     override fun onBackPressed() {
         if (!router.handleBack()) {
             super.onBackPressed()
         }
+    }
+
+    private fun initNavigationDrawer() {
+        val newDeck = PrimaryDrawerItem().withIdentifier(1).withName("New Deck")
+        val editDeck = PrimaryDrawerItem().withIdentifier(2).withName("Edit Deck")
+        val export = PrimaryDrawerItem().withIdentifier(3).withName("Export")
+        val settings = SecondaryDrawerItem().withIdentifier(4).withName("Settings")
+
+        val result = DrawerBuilder()
+                .withActivity(this)
+                .withToolbar(toolbar)
+                .addDrawerItems(
+                        newDeck,
+                        editDeck,
+                        export,
+                        DividerDrawerItem(),
+                        settings
+                )
+                .withOnDrawerItemClickListener { view, position, drawerItem ->
+                    when (drawerItem.identifier.toInt()) {
+                        1 -> pushController(FactionSelectController::class.java)
+                        2 -> pushController(DeckbuildController::class.java)
+                        3 -> Log.d(TAG, "Export item selected but not yet implemented.")
+                        4 -> Log.d(TAG, "Settings item selected but not yet implemented.")
+                    }
+                    true
+                }
+                .build()
+    }
+
+    private fun pushController(controller: Class<out Controller>) {
+
     }
 
 }
