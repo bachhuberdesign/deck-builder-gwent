5cd9fdfec08d609397f0723d6a19e36312eb0ad4
Eric Bachhuber <bachhuberdesign@gmail.com>
2017-04-20 12:50:41 -0500

    Added power totals display for lanes

---
+++ app/src/main/java/com/bachhuberdesign/gwentcardviewer/features/deckbuild/LaneTotals.kt
@@ -0,0 +1,11 @@
+package com.bachhuberdesign.gwentcardviewer.features.deckbuild
+
+/**
+ * @author Eric Bachhuber
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+data class LaneTotals(var meleeTotal: Int = 0,
+                      var rangedTotal: Int = 0,
+                      var siegeTotal: Int = 0,
+                      var eventTotal: Int = 0)
