40e6b8a6be54ce21264033a7293d3f2e0ee0d589
Eric Bachhuber <bachhuberdesign@gmail.com>
2017-05-09 10:53:33 -0500

    Minor refactoring

--- app/src/main/java/com/bachhuberdesign/deckbuildergwent/features/deckbuild/DeckRepository.kt
+++ app/src/main/java/com/bachhuberdesign/deckbuildergwent/features/deckbuild/DeckRepository.kt
@@ -1,7 +1,6 @@
 package com.bachhuberdesign.deckbuildergwent.features.deckbuild
 
 import android.content.ContentValues
-import android.database.Cursor
 import com.bachhuberdesign.deckbuildergwent.features.shared.exception.CardException
 import com.bachhuberdesign.deckbuildergwent.features.shared.exception.DeckException
 import com.bachhuberdesign.deckbuildergwent.features.shared.model.Card
@@ -27,13 +26,17 @@ import kotlin.collections.ArrayList
  */
 @PersistedScope
 class DeckRepository
-@Inject constructor(var gson: Gson,
-                    val database: BriteDatabase) {
+@Inject constructor(var gson: Gson, val database: BriteDatabase) {
 
     companion object {
         @JvmStatic val TAG: String = DeckRepository::class.java.name
     }
 
+    /**
+     * Checks if a given [deckName] has been persisted in the database.
+     *
+     * @return True if a deck with [deckName] is persisted. False if no deck with [deckName] is persisted.
+     */
     fun deckNameExists(deckName: String): Boolean {
         var normalizedDeckName = Normalizer.normalize(deckName, Normalizer.Form.NFD).replace("[^\\p{ASCII}]", "")
         normalizedDeckName = normalizedDeckName.replace("'", "''")
@@ -43,6 +46,9 @@ class DeckRepository
         return count.simpleQueryForLong() > 0
     }
 
+    /**
+     * @return [Deck]
+     */
     fun getDeckById(deckId: Int): Deck? {
         if (deckId <= 0) {
             throw DeckException("Expected a valid deck id but received $deckId.")
@@ -67,6 +73,9 @@ class DeckRepository
         return deck
     }
 
+    /**
+     *
+     */
     fun observeRecentlyUpdatedDecks(): Observable<MutableList<Deck>> {
         val query = "SELECT * FROM ${Deck.TABLE} " +
                 "ORDER BY last_update DESC " +
@@ -77,6 +86,9 @@ class DeckRepository
                 .observeOn(AndroidSchedulers.mainThread())
     }
 
+    /**
+     *
+     */
     fun observeCardUpdates(deckId: Int): Observable<MutableList<Card>> {
         val tables: MutableList<String> = arrayListOf(Card.TABLE, "user_decks_cards")
         val query: String = "SELECT * FROM ${Card.TABLE} " +
@@ -91,6 +103,9 @@ class DeckRepository
                 .observeOn(AndroidSchedulers.mainThread())
     }
 
+    /**
+     *
+     */
     fun getCardsForDeck(deckId: Int): MutableList<Card> {
         val cursor = database.query("SELECT * FROM ${Card.TABLE} " +
                 "JOIN user_decks_cards as t2 " +
@@ -108,6 +123,9 @@ class DeckRepository
         return cards
     }
 
+    /**
+     * @return [Card]
+     */
     private fun getLeaderCardForDeck(leaderCardId: Int): Card {
         val cursor = database.query("SELECT * FROM ${Card.TABLE} WHERE ${Card.ID} =  $leaderCardId")
 
@@ -120,6 +138,9 @@ class DeckRepository
         throw CardException("Leader $leaderCardId not found.")
     }
 
+    /**
+     * @return
+     */
     fun getAllUserCreatedDecks(): List<Deck> {
         val cursor = database.query("SELECT * FROM ${Deck.TABLE}")
         val decks: MutableList<Deck> = ArrayList()
@@ -137,6 +158,9 @@ class DeckRepository
         return decks
     }
 
+    /**
+     * @return [Int]
+     */
     fun countUserDecksWithLeader(leaderCardId: Int): Int {
         val cursor = database.query("SELECT * FROM ${Deck.TABLE} " +
                 "WHERE ${Deck.LEADER_ID} = $leaderCardId")
@@ -146,14 +170,29 @@ class DeckRepository
         }
     }
 
-    fun getFactions(): Cursor {
-        return database.query("SELECT * FROM ${Faction.TABLE}")
+    /**
+     *
+     */
+    fun getFactions(): List<Faction> {
+        val factions: MutableList<Faction> = ArrayList()
+        val factionsCursor = database.query("SELECT * FROM ${Faction.TABLE}")
+
+        factionsCursor.use {
+            while (factionsCursor.moveToNext()) {
+                factions.add(Faction.MAPPER.apply(factionsCursor))
+            }
+        }
+
+        factions.forEach { faction ->
+            faction.leaders = getLeadersForFaction(faction.id) as MutableList<Card>
+        }
+
+        return factions
     }
 
-    fun getLeaders(): Cursor {
-        return database.query("SELECT * FROM ${Card.TABLE} WHERE ${Card.TYPE} = ${CardType.LEADER}")
-    }
-
+    /**
+     *
+     */
     fun getLeadersForFaction(factionId: Int): List<Card> {
         val cursor = database.query("SELECT * FROM ${Card.TABLE} " +
                 "WHERE ${Card.TYPE} = ${CardType.LEADER} " +
@@ -169,6 +208,9 @@ class DeckRepository
         return leaders
     }
 
+    /**
+     *
+     */
     fun saveDeck(deck: Deck): Int {
         val currentTime = Date().time
         val deckValues = ContentValues()
@@ -190,6 +232,9 @@ class DeckRepository
         return deck.id
     }
 
+    /**
+     *
+     */
     fun updateLeaderForDeck(deckId: Int, leaderId: Int) {
         val deckValues = ContentValues()
         deckValues.put(Deck.LEADER_ID, leaderId)
@@ -197,6 +242,9 @@ class DeckRepository
         database.update(Deck.TABLE, deckValues, "${Deck.ID} = $deckId")
     }
 
+    /**
+     *
+     */
     fun addCardToDeck(card: Card, deckId: Int) {
         val values = ContentValues()
         values.put("card_id", card.cardId)
@@ -213,6 +261,9 @@ class DeckRepository
         saveDeckLastUpdateTime(deckId)
     }
 
+    /**
+     *
+     */
     fun renameDeck(newDeckName: String, deckId: Int) {
         val values = ContentValues()
         values.put(Deck.NAME, newDeckName)
@@ -220,6 +271,9 @@ class DeckRepository
         database.update(Deck.TABLE, values, "${Deck.ID} = $deckId")
     }
 
+    /**
+     *
+     */
     fun removeCardFromDeck(card: Card, deckId: Int) {
         val query: String
 
@@ -243,6 +297,9 @@ class DeckRepository
         saveDeckLastUpdateTime(deckId)
     }
 
+    /**
+     *
+     */
     fun deleteDeck(deckId: Int) {
         database.delete(Deck.JOIN_CARD_TABLE, "deck_id = $deckId")
         database.delete(Deck.TABLE, "${Deck.ID} = $deckId")
--- app/src/main/java/com/bachhuberdesign/deckbuildergwent/features/factionselect/FactionSelectPresenter.kt
+++ app/src/main/java/com/bachhuberdesign/deckbuildergwent/features/factionselect/FactionSelectPresenter.kt
@@ -2,7 +2,6 @@ package com.bachhuberdesign.deckbuildergwent.features.factionselect
 
 import com.bachhuberdesign.deckbuildergwent.features.deckbuild.DeckRepository
 import com.bachhuberdesign.deckbuildergwent.features.shared.base.BasePresenter
-import com.bachhuberdesign.deckbuildergwent.features.shared.model.Card
 import com.bachhuberdesign.deckbuildergwent.features.shared.model.Faction
 import com.bachhuberdesign.deckbuildergwent.inject.annotation.PersistedScope
 import javax.inject.Inject
@@ -20,43 +19,13 @@ class FactionSelectPresenter
         @JvmStatic val TAG: String = FactionSelectPresenter::class.java.name
     }
 
-    override fun attach(view: FactionSelectMvpContract) {
-        super.attach(view)
-    }
-
-    override fun detach() {
-        super.detach()
-    }
-
     /**
      *
      */
     fun loadFactions() {
-        val factions: MutableList<Faction> = ArrayList()
-        val factionsCursor = repository.getFactions()
-        val leadersCursor = repository.getLeaders()
+        val factions: List<Faction> = repository.getFactions()
 
-        factionsCursor.use {
-            while (factionsCursor.moveToNext()) {
-                factions.add(Faction.MAPPER.apply(factionsCursor))
-            }
-        }
-
-        leadersCursor.use {
-            while (leadersCursor.moveToNext()) {
-                val card = Card.MAPPER.apply(leadersCursor)
-                factions.forEachIndexed { index, faction ->
-                    if (card.faction == faction.id) {
-                        factions[index].leaders.add(card)
-                    }
-                }
-            }
-        }
-
-        if (isViewAttached()) {
-            view!!.onFactionsLoaded(factions)
-        }
-
+        getViewOrThrow().onFactionsLoaded(factions)
     }
 
 
